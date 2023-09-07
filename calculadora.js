d94a817bee723f3462c5814115ac3d7da2ddb87b
dduick <m.penalozacastro@pm.me>
2023-09-07 11:42:35 -0300

    Nuvos cambios

--- calculadora.js
+++ calculadora.js
@@ -1,6 +1,6 @@
 const prompt = require("prompt-sync")();
 var operacion, numero1, numero2
-operacion = parseInt(prompt("Ingrese La opción deseada: 1.- Suma 2.- Resta 3.- Multiplicacion 4.- División "));
+operacion = parseInt(prompt("Ingrese La opción deseada: 1.- Suma 2.- Resta 3.- Multiplicacion 4.- División 5. ¿Quieres salir? "));
 switch (operacion) {
     case 1:
     console.log("Estas en suma: ")
@@ -29,5 +29,9 @@ switch (operacion) {
     numero2 = parseInt(prompt("Ingresa un número: "))
     resultado = numero1 / numero2
     console.log(resultado)
+
+    case 5:
+        operacion !== "s";
+        break;
 }
 /* Este esta editado por mi*/
